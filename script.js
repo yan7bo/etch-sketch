0e98e0f5c8cd9eb5207948efc13d35b20a07d296
Bo Yan <yan7bo@gmail.com>
2023-12-02 23:08:51 -0500

    Adds grid size buttons and adds option buttons
    Grid size buttons allows the user to resize the grid. Option buttons
    are to-do. Reset button is completed which allows the user to reset the
    sketch board back to white.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const INIT_GRID_SIZE = 16;
 const GRID_BORDER = "4px solid black";
+const GRID_SCALE_FACTOR = 1.5;
 
 const DIV_SIZE = 16;
-const DIV_BORDER_SIZE = 1;
+const DIV_BORDER_SIZE = 0;
 const DIV_BORDER = DIV_BORDER_SIZE + "px solid grey";
 const DIV_COLOR = "grey";
 
 const body = document.querySelector("body");
 
-function createGrid() {
+function createGrid(gridSize = INIT_GRID_SIZE, divSize = DIV_SIZE) {
     // initialize a 16x16 grid of square divs
     // divs will be 16px x 16px
 
@@ -16,24 +17,26 @@ function createGrid() {
     const divGrid = document.createElement("div");
     body.appendChild(divGrid);
     divGrid.setAttribute("id", "divGrid");
-    divGrid.style.height = ((DIV_SIZE + 2 * DIV_BORDER_SIZE) * INIT_GRID_SIZE) + "px";
-    divGrid.style.width = ((DIV_SIZE + 2 * DIV_BORDER_SIZE) * INIT_GRID_SIZE) + "px";
+    divGrid.style.height = ((divSize + 2 * DIV_BORDER_SIZE) * gridSize) * GRID_SCALE_FACTOR + "px";
+    divGrid.style.width = ((divSize + 2 * DIV_BORDER_SIZE) * gridSize) * GRID_SCALE_FACTOR + "px";
     divGrid.style.border = GRID_BORDER;
     divGrid.style.display = "flex";
     divGrid.style.flexWrap = "wrap";
 
 
     // creates a grid of smallDiv
-    for(let i = 0; i < INIT_GRID_SIZE; i++) {
-        for(let j = 0; j < INIT_GRID_SIZE; j++) {
+    for(let i = 0; i < gridSize; i++) {
+        for(let j = 0; j < gridSize; j++) {
             const smallDiv = document.createElement("div");
             smallDiv.setAttribute("class", "smallDiv");
-            smallDiv.style.height = DIV_SIZE + "px";
-            smallDiv.style.width = DIV_SIZE + "px";
-            smallDiv.style.border = "1px solid grey";
+            smallDiv.style.height = divSize * GRID_SCALE_FACTOR + "px";
+            smallDiv.style.width = divSize * GRID_SCALE_FACTOR + "px";
+            smallDiv.style.border = DIV_BORDER_SIZE + "px solid grey";
             divGrid.appendChild(smallDiv);
         }
     }
+
+    return divGrid;
 }
 
 const divGrid = document.querySelector("#divGrid");
@@ -45,22 +48,72 @@ function hover(event) {
     }
 }
 
-function main() {
-    createGrid();
+function resizeGrid(event) {
+    // check which button clicked
+    let userGridSize = +event.target.textContent.split("x")[0];
+    console.log(userGridSize);
+    /*
+    let userGridSize = +prompt("Please enter grid size (min 1, max 100): ", "16");
+    while(!(userGridSize <= 100 && userGridSize > 0)) {
+        userGridSize = +prompt("Invalid input! Please enter grid size (min 1, max 100): ", "16");
+    }
+    */
+    // console.log((typeof userGridSize) + userGridSize);
 
-    // checks for mouse down for sketching
-    // console.log(divGrid);
+    // delete current divGrid
     const divGrid = document.querySelector("#divGrid");
-    // console.log(divGrid);
-    divGrid.addEventListener("mousedown", () => {
-        divGrid.addEventListener("mouseover", hover);
+    divGrid.remove();
+
+    // create new divGrid
+    let userDivSize = (INIT_GRID_SIZE / userGridSize) * DIV_SIZE;
+    const newDivGrid = createGrid(userGridSize, userDivSize);
+
+    // add sketching functionality to new divGrid
+    addSketch(newDivGrid);
+}
+
+function addSketch(element) {
+    // checks for mouse down for sketching
+    element.addEventListener("mousedown", () => {
+        element.addEventListener("mouseover", hover);
     });
 
     // checks for mouse up to stop sketching
     addEventListener("mouseup", () => {
-        console.log("mouse up");
-        divGrid.removeEventListener("mouseover", hover);
+        element.removeEventListener("mouseover", hover);
     })
+}
+
+function doReset() {
+    const listSmallDiv = document.querySelectorAll(".smallDiv");
+    listSmallDiv.forEach((currentValue) => {
+        currentValue.style.backgroundColor = "white";
+    })
+}
+
+function getOption(event) {
+    let option = event.target.textContent;
+    if(option == "Reset") {
+        doReset();
+    } else if(option == "Rainbow") {
+        // do rainbow
+    } else if(option == "10% Opacity") {
+        // do opacity
+    }
+}
+
+function main() {
+    const divGrid = createGrid();
+
+    addSketch(divGrid);
+
+    // event to resize grid
+    const listBtns = document.querySelector("#listBtns");
+    listBtns.addEventListener("click", resizeGrid);
+
+    // option buttons
+    const optionBtns = document.querySelector("#optionBtns");
+    optionBtns.addEventListener("click", getOption);
 }
 
 main();
